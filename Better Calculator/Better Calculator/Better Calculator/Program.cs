61cfff97cf70a106566254612fbd5c8bf25bb425
Atilhan <gorinchem1995@hotmail.com>
2022-11-21 13:15:59 +0100

    Better calculator project, except terminal doesnt seem to be working. Need to clarify yet why

---
+++ Better Calculator/Better Calculator/Better Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert a number: ");
+            double number_1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insert a operator: ");
+            string chose_operator = Console.ReadLine();
+
+            Console.Write("Insert a second number: ");
+            double number_2 = Convert.ToDouble(Console.ReadLine());
+
+            if (chose_operator == "+")
+            {
+                Console.WriteLine(number_1 + number_2);
+            }
+            else if (chose_operator == "-")
+            {
+                Console.WriteLine(number_1 - number_2);
+            }
+            else if(chose_operator == "*")
+            {
+                Console.WriteLine(number_1 * number_2);
+            }
+            else if(chose_operator == "/")
+            {
+                Console.WriteLine(number_1 / number_2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+        }
+    }
+}
